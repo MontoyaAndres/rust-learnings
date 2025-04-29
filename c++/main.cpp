2b8c5441edc0ee4b702aa14f82c41ccf4a051a0c
MontoyaAndres <andresmontoyafcb@gmail.com>
2025-04-29 17:55:13 -0500

    Feat: c++

---
+++ c++/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int age = 0;
+  const char letra = 'A';
+  int list_ages[] = {18, 19, 20, 21, 22, 23, 24, 25, 26, 27};
+
+  cout << age << endl;
+  cout << letra;
+}
