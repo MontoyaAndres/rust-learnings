805448175b5f12b057cb033502679c9a6837ad64
MontoyaAndres <andresmontoyafcb@gmail.com>
2025-04-30 12:57:15 -0500

    Feat: calculator fn

--- platzi-curso/src/calculator.rs
+++ platzi-curso/src/calculator.rs
@@ -1,49 +1,51 @@
 use regex::Regex;
+use std::str::FromStr;
+
+const ADDITION: &str = "(\\d+)\\s*?(\\+)\\s*?(\\d+)";
+const SUBTRACTION: &str = "(\\d+)\\s*?(\\-)\\s*?(\\d+)";
+const MULTIPLICATION: &str = "(\\d+)\\s*?(\\*)\\s*?(\\d+)";
+const DIVISION: &str = "(\\d+)\\s*?(/)\\s*?(\\d+)";
+
+fn operations(mut expression: String, operator: &str) -> String {
+    let re = Regex::from_str(operator).unwrap();
+
+    loop {
+        let caps = re.captures(&expression);
+
+        if caps.is_none() {
+            break;
+        }
+
+        let caps = caps.unwrap();
+
+        let cap_expression = caps.get(0).unwrap().as_str();
+        let left_value: i32 = caps.get(1).unwrap().as_str().parse().unwrap();
+        let operator = caps.get(2).unwrap().as_str();
+        let right_value: i32 = caps.get(3).unwrap().as_str().parse().unwrap();
+
+        let result = match operator {
+            "+" => left_value + right_value,
+            "*" => left_value * right_value,
+            "-" => left_value - right_value,
+            "/" => left_value / right_value,
+            _ => panic!("Unknown operator"),
+        };
+
+        expression = expression.replace(cap_expression, &result.to_string());
+    }
+
+    expression.to_string()
+}
 
 fn main() {
-    let re_add = Regex::new(r"(\d+)\s*\+\s*(\d+)").unwrap();
-    let re_multiply = Regex::new(r"(\d+)\s*\*\s*(\d+)").unwrap();
-
     println!("Enter expression:");
-
     let mut expression = String::new();
     std::io::stdin().read_line(&mut expression).unwrap();
 
-    loop {
-        let caps = re_multiply.captures(expression.as_str());
-
-        if caps.is_none() {
-            break;
-        }
-
-        let caps = caps.unwrap();
-
-        let cap_expression = caps.get(0).unwrap().as_str();
-        let left_value: i32 = caps.get(1).unwrap().as_str().parse().unwrap();
-        let right_value: i32 = caps.get(2).unwrap().as_str().parse().unwrap();
-
-        let result = left_value * right_value;
-
-        expression = expression.replace(cap_expression, &result.to_string());
-    }
-
-    loop {
-        let caps = re_add.captures(expression.as_str());
-
-        if caps.is_none() {
-            break;
-        }
-
-        let caps = caps.unwrap();
-
-        let cap_expression = caps.get(0).unwrap().as_str();
-        let left_value: i32 = caps.get(1).unwrap().as_str().parse().unwrap();
-        let right_value: i32 = caps.get(2).unwrap().as_str().parse().unwrap();
-
-        let result = left_value + right_value;
-
-        expression = expression.replace(cap_expression, &result.to_string());
-    }
+    expression = operations(expression, ADDITION);
+    expression = operations(expression, MULTIPLICATION);
+    expression = operations(expression, SUBTRACTION);
+    expression = operations(expression, DIVISION);
 
     println!("Result: {}", expression);
 }
